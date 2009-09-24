ff78816386d857b62d6a914d95381f5eb2649f19
Yoichiro Hasebe <yohasebe@gmail.com>
2009-09-24 17:39:57 +0900

    nodesep and ranksep settings are added; pdf and svg output added

--- lib/rubyfca.rb
+++ lib/rubyfca.rb
@@ -327,8 +327,9 @@ class FormalContext
   ## For options, see 'rubyfca'
   def generate_dot(opts)
     index_max_width = @concepts.size.to_s.split(//).size
-
-    clattice = RubyGraphviz.new("clattice", :rankdir => "", :nodesep => "0.4", :ranksep => "0.2")
+     nodesep = @opts[:nodesep] ? @opts[:nodesep].to_s : "0.4"
+     ranksep = @opts[:ranksep] ? @opts[:ranksep].to_s : "0.2"
+    clattice = RubyGraphviz.new("clattice", :rankdir => "", :nodesep => nodesep, :ranksep => ranksep)
 
     if opts[:circle] and opts[:legend]
       legend   = RubyGraphviz.new("legend", :rankdir => "TB", :lebelloc => "t", :centered => "false")
